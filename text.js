982a965ec2346b829ab53fb84c730b6403ddfba7
molaq <molaq@github.com>
2023-12-10 00:48:31 +0100

    Remove commented out code for displaying text

--- text.js
+++ text.js
@@ -4,11 +4,3 @@ function displayText(ctx, text, x, y, color = 'white', fontSize = 20, align = 'c
     ctx.textAlign = align;
     ctx.fillText(text, x, y);
 }
-
-// function displayText(canvas, text, x, y, color = 'white', fontSize = 20, align = 'center') {
-//     ctx = canvas.getContext('2d');
-//     ctx.font = `${fontSize}px Arial`;
-//     ctx.fillStyle = color;
-//     ctx.textAlign = align;
-//     ctx.fillText(text, x, y);
-// }
